ac6ef042e600faa5d264a16a556bb7774dc2b138
ionut <ionut@DESKTOP-J9N3UPE.pb>
2019-12-22 16:31:49 +0000

    making the code inside the course programme class

--- src/main/java/sampletest/courseProgrammeClass.java
+++ src/main/java/sampletest/courseProgrammeClass.java
@@ -1,5 +1,51 @@
 package sampletest;
 
+import java.util.ArrayList;
+
 public class courseProgrammeClass {
 
+	String CourseName;
+	String startDate;
+	String endDate;
+	ArrayList<String> moduleList = new ArrayList<String>();
+	ArrayList<String> StudentList = new ArrayList<String>();
+	
+	public ArrayList<String> getStudentList() {
+	    return StudentList;
+	  }
+
+	public ArrayList<String> getModuleList() {
+	    return moduleList;
+	  }
+	
+	public void setCourseName(String CourseName) {
+		CourseName = CourseName;
+
+	}
+
+	public String getCourseName() {
+	      return CourseName;
+
+	}
+	
+	public void setStartDate(String startDate) {
+		startDate = startDate;
+
+	}
+
+	public String getStartDate() {
+	      return startDate;
+
+	}
+	
+	public void setEndDate(String endDate) {
+		endDate = endDate;
+
+	}
+
+	public String getEndDate() {
+	      return endDate;
+
+	}
+	
 }
