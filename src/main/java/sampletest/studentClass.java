7886f32a3ce7a97dc7c06d94cdc757add0c4dae0
ionut <ionut@DESKTOP-J9N3UPE.pb>
2019-12-22 16:08:59 +0000

    adding code for the student class

--- src/main/java/sampletest/studentClass.java
+++ src/main/java/sampletest/studentClass.java
@@ -1,5 +1,62 @@
 package sampletest;
 
 public class studentClass {
+	
+	String Name;
+	int Age;
+	String DOB;
+	int ID;
+	String username;
+
+	public void setName(String Name) {
+	      Name = Name;
+
+	}
+
+	public String getName() {
+	      return Name;
+
+	}
+
+	public void setAge(int Age) {
+	      Age = Age;
+
+	}
+
+	public int getAge() {
+	      return Age;
+
+	}
+	
+	public void setDOB(String DOB) {
+	      DOB = DOB;
+
+	}
+
+	public String getDOB() {
+	      return DOB;
+
+	}
+	
+	public void setUserName(String username) {
+	      username = username;
+
+	}
+
+	public String getUserName() {
+	      return username + Age;
+
+	}
+	
+	public void setID(int ID) {
+	      ID = ID;
+
+	}
+
+	public int getID() {
+	      return ID;
+
+	}
+	
 
 }
