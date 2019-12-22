241459edb9e9223fbe9a47e50ac65c7329ddd1a2
ionut <ionut@DESKTOP-J9N3UPE.pb>
2019-12-22 16:24:21 +0000

    adding the code for module class

--- src/main/java/sampletest/moduleClass.java
+++ src/main/java/sampletest/moduleClass.java
@@ -1,5 +1,46 @@
 package sampletest;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 public class moduleClass {
 
+
+	String moduleName;
+	String moduleID;
+	ArrayList<String> AssociatedCourses = new ArrayList<String>();
+	ArrayList<String> StudentList = new ArrayList<String>();
+
+	
+	
+	public void setModuleName(String moduleName) {
+		moduleName = moduleName;
+
+	}
+
+	public String getModuleName() {
+	      return moduleName;
+
+	}
+	
+	
+	public void setModuleID(String moduleID) {
+		moduleID = moduleID;
+
+	}
+
+	public String getModuleID() {
+	      return moduleID;
+
+	
+	}
+	
+	public ArrayList<String> getAssociatedCourseslist() {
+	    return AssociatedCourses;
+	  }
+	
+	public ArrayList<String> getStudentList() {
+	    return StudentList;
+	  }
+	
 }
